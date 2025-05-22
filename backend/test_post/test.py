872aea292cb9cafd83de612f935ca2814b43879b
Koozie24 <jake.mckenzie2017@gmail.com>
2025-05-21 22:14:10 -0400

    updated routing to html pages

--- backend/test_post/test.py
+++ backend/test_post/test.py
@@ -1,9 +1,5 @@
-print("test.py is executing...")
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os 
-
-print("Flask imported successfully...")
 
 app = Flask(__name__)
 
@@ -11,14 +7,28 @@ TEMPLATES_DIR = os.path.abspath("frontend/templates")
 STATIC_DIR = os.path.abspath("frontend/static")
 app= Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATIC_DIR)
 
-print("templates_dir: ", TEMPLATES_DIR)
-
+#defining routes for html pages in templates
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/cuts')
+def cuts():
+    return render_template('cuts.html')
+@app.route('/identify')
+def identify():
+    return render_template('identify.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/plans')
+def plans():
+    return render_template('plans.html')
 
-
-
+@app.route('/handle_submit_cut_form', methods=['POST'])
+def handle_submit_cut_form():
+    kerf_size = request.form.get('kerf-type')
+    
+    print(kerf_size)
 if __name__ == "__main__":
     print("Running Flask server...")
     app.run(debug=True)
